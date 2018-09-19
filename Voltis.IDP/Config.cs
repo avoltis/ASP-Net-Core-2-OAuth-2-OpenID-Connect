ae1dc643add3db9fe97f952fc4f0a1e8ae561a56
vagolli <v.agolli@live.com>
2018-09-19 14:03:18 +0200

    configure IdentityServer with Hybrid Flow

--- Voltis.IDP/Config.cs
+++ Voltis.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
@@ -54,7 +55,33 @@ namespace Voltis.IDP
 
         public static IEnumerable<Client> GetClients()
         {
-            return new List<Client>();
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44347/signin-oidc"
+                    },
+                    //PostLogoutRedirectUris = new List<string>()
+                    //{
+                    //    "https://localhost:44347/signout-callback-oidc"
+                    //},
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+             };
+
         }
     }
 }
